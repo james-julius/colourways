d97fbe9a148b14948e3daa97b657bd0144f452c1
James <jamesdarby7@gmail.com>
2020-05-16 12:30:20 +0100

    Quite a few changes. Prettier added, css changes, much more dynamic text and colour updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './App.scss';
 import hslWheel from './hslwheel.png';
-import {convert as colorConvert } from 'color-convert';
+import convert from 'color-convert';
 
 function App() {
   const [scheme, setScheme] = useState('monochromatic');
@@ -10,44 +10,89 @@ function App() {
   const [secondaryColor, setSecondaryColor] = useState({backgroundColor: 'rebeccapurple'});
   const [secondaryFontColor, setSecondaryFontColor] = useState({color: 'black'});
   const [tertiaryColor, setTertiaryColor] = useState({backgroundColor: 'white'});
-  const [colorFormat, setColorFormat] = useState('hsla');
+  const [currentColors, setCurrentColors] = useState([primaryColor.backgroundColor, secondaryColor.backgroundColor, tertiaryColor.backgroundColor]);
+  const [colorFormat, setColorFormat] = useState('hsl');
   const [colorFormatHandlePosition, setColorFormatHandlePosition] = useState({});
+  const [dynamicTextColor, setDynamicTextColor] = useState({color: 'black'});
+  const primaryRef = useRef();
+  const secondaryRef = useRef();
+  const tertiaryRef = useRef();
+  
+  const handleCopy = (inputRef) => {
+    navigator.clipboard.writeText(inputRef.current.innerHTML);
+    inputRef.current.innerHTML = 'Copied!'
+  }
 
-  const changeColorFormat = (format) => {
-    console.log(format)
-    console.log('format change called with format: ' + format)
+  const handleMouseEnter = (inputRef) => {
+    console.log(inputRef)
+    inputRef.current.innerHTML = 'Double click to copy.';
+  }
+  const handleMouseLeave = (inputRef, refIndex) => {
+    console.log(currentColors)
+    console.log('REFINDEX: ',refIndex);
+    console.log(currentColors[refIndex])
+    inputRef.current.innerHTML = handleColorFormat(currentColors[refIndex]);
+  }
+
+  const changeColorFormat = useCallback((format) => {
+    // Animating the slider switch to the selected format
+    // Syntax change is handled by the below function, that returns directly to the element.
     let leftValue;
     switch (format) {
-      case 'hsla':
-        leftValue = 'calc(12.5%-8px)';
+      case 'hsl':
+        setColorFormat('hsl');
+        leftValue = 'calc(12.5% - 8px)';
         break;
       case 'rgb': 
-        leftValue = 'calc(37.5%-8px)';
+      setColorFormat('rgb');
+        leftValue = 'calc(37.5% - 8px)';
         break;
       case 'cmyk':
-        leftValue = 'calc(62.5%-8px)';
+        setColorFormat('cmyk');
+        leftValue = 'calc(62.5% - 8px)';
         break;
       case 'hex':
-        leftValue = 'calc(97.5%-8px)';
+        setColorFormat('hex');
+        leftValue = 'calc(87.5% - 8px)';
         break;
     }
-    console.log(leftValue)
+    // console.log(leftValue)
     setColorFormatHandlePosition({
       position: 'absolute',
       left: leftValue
     });
-  }
+  }, [colorFormat]);
 
-  const hsla = (hue, saturation, lightness, opacity) => {
-    return `hsla(${hue},${saturation}, ${lightness + '%'}, ${opacity})`;
+  const handleColorFormat = useCallback((inputHsl) => {
+    // Function is called on render
+    // console.log('INPUT HSL: ', inputHsl);
+    // console.log(colorFormat)
+    const hslArr = inputHsl.slice(5, inputHsl.length -1).replace(/%/g, '').split`,`.map(str => +str);
+    if (colorFormat === 'hsl') {
+      return inputHsl}
+    if (colorFormat === 'rgb') {
+      let [r,g,b] = convert.hsl.rgb(hslArr);
+      return `rgb(${r},${g},${b})`}
+    if (colorFormat === 'cmyk') {
+      let [c,m,y,k] = convert.hsl.cmyk(hslArr);
+      return `cmyk(${c},${m},${y},${k})`}
+    if (colorFormat === 'hex') {
+      // console.log(convert.hsl.hex)
+      return '#' + convert.hsl.hex(hslArr)}
+  }, [colorFormat]);
+
+  const hsl = (hue, saturation, lightness) => {
+    return `hsl(${hue},${saturation}, ${lightness + '%'})`;
   };
 
   const colourChanger = () => {
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
+    let [h,s,l] = [Math.floor(Math.random()*360),100,Math.floor(Math.random()* 100)];
+    if (l >= 50 ) {setDynamicTextColor({color: 'rgb(50,50,50)'})} else {setDynamicTextColor({color: 'rgb(240,240,240)'})}
     document.getElementsByClassName('main')[0].style.backgroundColor = `rgba(${r},${g},${b}, 0.3)`;
-    document.getElementsByClassName('sidebar')[0].style.backgroundColor = `rgba(${r},${g},${b},1)`;
+    document.getElementsByClassName('sidebar')[0].style.backgroundColor = `hsl(${h},${s}%,${l}%)`;
   }
 
   const applyColorScheme = () => {
@@ -60,9 +105,9 @@ function App() {
         Math.floor(Math.random()*50),
         Math.floor(Math.random()*20)+80];
       console.log(lightness);
-      const primary = hsla(hue,'100%',lightness[0], 1);
-      const secondary = hsla(hue,'100%',lightness[1], 1);
-      const tertiary = hsla(hue,'100%',lightness[2], 1);
+      const [primary,secondary,tertiary] = [hsl(hue,'100%',lightness[0]),hsl(hue,'100%',lightness[1]),hsl(hue,'100%',lightness[2])];
+      console.log([primary,secondary,tertiary]);
+      setCurrentColors([primary,secondary,tertiary]);
       setPrimaryColor({
         color: tertiary,
         backgroundColor: primary
@@ -78,15 +123,15 @@ function App() {
 
   useEffect(() => {
     colourChanger();
-    setInterval(colourChanger, 5000);
+    setInterval(colourChanger, 15000);
     // colourChanger();
   }, []);
 
   return (
   <div className="App">
     <div className="sidebar">
-      <h1>Colourways</h1>
-      <h2>A colour scheme generator for websites.</h2>
+      <h1 style={dynamicTextColor}>Colourways</h1>
+      <h2 style={dynamicTextColor}>A colour scheme generator for websites.</h2>
       <select onChange={(event) => setScheme(event.target.value)}>
         <option value="monochromatic">Monochromatic</option>
         <option value="complementary">Complementary</option>
@@ -94,10 +139,10 @@ function App() {
       </select>
       <div className="colourButton" onClick={applyColorScheme}>Generate</div>
       <div className="colourway">
-        <h3>Format:</h3>
-        <span className="switch" onClick={changeColorFormat}>
+        <h3 style={dynamicTextColor}>Format:</h3>
+        <span className="switch">
           <div className="slider">
-            <div className="handleContainer" onClick={() => changeColorFormat('hsla')}>
+            <div className="handleContainer" onClick={() => changeColorFormat('hsl')}>
               <div className="handleoverlay"/>
             </div>
             <div className="handleContainer" onClick={() => changeColorFormat('rgb')}>
@@ -111,24 +156,36 @@ function App() {
             </div>
             <div className="handle" style={colorFormatHandlePosition}/>
           </div>
-          <div className="options">
+          <div className="options" style={dynamicTextColor}>
             <p>HSL</p>
             <p>RGB</p>
             <p>CMYK</p>
             <p>HEX</p>
           </div>
         </span>
-        <div className="colorBar" style={primaryColor} onClick={() => console.log('single clicked')} onDoubleClick={() => {console.log('double clicked')}}>
-            {primaryColor.backgroundColor}
+        <div className="colorBar" ref={primaryRef} 
+          style={primaryColor} 
+          onMouseEnter={() => handleMouseEnter(primaryRef)} 
+          onMouseLeave={() => handleMouseLeave(primaryRef,0)} 
+          onDoubleClick={() => handleCopy(primaryRef)}>
+            {handleColorFormat(primaryColor.backgroundColor)}
         </div>
-        <div className="colorBar" style={secondaryColor}>
+        <div className="colorBar" ref={secondaryRef} 
+          style={secondaryColor} 
+          onMouseEnter={() => handleMouseEnter(secondaryRef)} 
+          onMouseLeave={() => handleMouseLeave(secondaryRef,1)} 
+          onDoubleClick={() => handleCopy(secondaryRef)}>
           <span>
-            {secondaryColor.backgroundColor} 
+            {handleColorFormat(secondaryColor.backgroundColor)} 
           </span>
         </div>
-        <div className="colorBar" style={tertiaryColor}>
+        <div className="colorBar" ref={tertiaryRef} 
+          style={tertiaryColor} 
+          onMouseEnter={() => handleMouseEnter(tertiaryRef)} 
+          onMouseLeave={() => handleMouseLeave(tertiaryRef,2)} 
+          onDoubleClick={() => handleCopy(tertiaryRef)}>
           <span>
-            {tertiaryColor.backgroundColor}
+            {handleColorFormat(tertiaryColor.backgroundColor)}
           </span>
         </div>
       </div>
